d84676c34f36150ed4918c1deb910a0479efb370
JhonHander <zzjanderz@gmail.com>
2025-05-02 11:19:45 -0500

    chore: Update repository files and add sonar-project.properties to gitignore

--- src/domain/entities/ClassroomFeature.js
+++ src/domain/entities/ClassroomFeature.js
@@ -1,6 +1,8 @@
-class ClassroomFeatures {
+class ClassroomFeature {
     constructor({ id, name }) {
         this.id = id; // ID of the classroom feature
         this.name = name; // Name of the classroom feature (e.g., projector, whiteboard, air conditioning)
     }
 }
+
+export default ClassroomFeature;
